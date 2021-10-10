29aef7ae98c6231bfa72ff70c22b422ec4326965
Nayem-Ul-Islam <nayemulislam606@gmail.com>
2021-10-10 23:45:41 +0600

    application

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\s your favourite color?',
+      'What\s your favourite animal?'
+    ];
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
